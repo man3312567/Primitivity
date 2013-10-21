f9db08b044301c2b0873881c3466789f715be91f
man3312567 <man3312567@gmail.com>
2013-10-21 16:23:35 -0400

    Basics
    Added basics of the mod

---
+++ man3312567/Primitivity/common/PrimitivityMod.java
@@ -0,0 +1,44 @@
+package man3312567.Primitivity.common; //The package your mod is in
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
+import cpw.mods.fml.common.SidedProxy;
+import man3312567.Primitivity.common.PrimitivityModCommonProxy;
+
+@NetworkMod(clientSideRequired=true,serverSideRequired=true, //Whether client side and server side are needed
+clientPacketHandlerSpec = @SidedPacketHandler(channels = {"PrimitivityMod"}, packetHandler = PrimitivityModClientPacketHandler.class), //For clientside packet handling
+serverPacketHandlerSpec = @SidedPacketHandler(channels = {"PrimitivityMod"}, packetHandler = PrimitivityModServerPacketHandler.class)) //For serverside packet handling
+
+//MOD BASICS
+@Mod(modid="PrimitivityMod",name="Primitivity Mod",version="Release")
+
+public class PrimitivityMod {
+
+@Instance("TutorialMod") //The instance, this is very important later on
+public static PrimitivityMod instance = new PrimitivityMod();
+
+@SidedProxy(clientSide = "man3312567.PrimitivityMod.client.PrimitivityModClientProxy", serverSide = "man3312567.TutorialMod.common.PrimitivityModCommonProxy") //Tells Forge the location of your proxies
+public static PrimitivityModCommonProxy proxy;
+
+@PreInit
+public void PreInit(FMLPreInitializationEvent e){
+
+}
+
+@Init
+public void InitTutorialMod(FMLInitializationEvent event){ //Your main initialization method
+
+//MULTIPLAYER ABILITY
+NetworkRegistry.instance().registerGuiHandler(this, proxy); //Registers the class that deals with GUI data
+
+}
+}
---
+++ man3312567/Primitivity/common/PrimitivityModClientProxy.java
@@ -0,0 +1,11 @@
+package man3312567.Primitivity.common;
+
+import man3312567.Primitivity.common.PrimitivityModCommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class PrimitivityModClientProxy extends PrimitivityModCommonProxy {
+
+public void registerRenderInformation(){
+}
+
+}
